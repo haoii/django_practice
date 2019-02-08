aef22660ce096b2ba0409567b6933cf490d2ce8b
haogaofeng <gf_hao@126.com>
2019-02-08 15:50:29 +0800

    1. 增加submit post的功能。

--- BigWaveBackend/square/views.py
+++ BigWaveBackend/square/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.utils import timezone
+from django.views.decorators.csrf import csrf_exempt
 
-from .models import SquarePost
+from .models import SquarePost, User
 
 
 def index(request):
@@ -19,7 +20,7 @@ def index(request):
             'pub_date': post.pub_date
         }
 
-    latest_posts = SquarePost.objects.order_by('-pub_date')[:5]
+    latest_posts = SquarePost.objects.order_by('-pub_date')[:10]
     latest_posts = [post_to_dict(p) for p in latest_posts]
     latest_posts_response = {
         'get_time': timezone.now(),
@@ -27,3 +28,25 @@ def index(request):
     }
 
     return JsonResponse(latest_posts_response)
+
+
+@csrf_exempt
+def submit_post(request):
+
+    post_text = request.POST.get("post_text")
+    user = User.objects.get(pk='haogaofeng')
+    post = SquarePost(user=user, post_text=post_text, image_url='', votes=0, pub_date=timezone.now())
+    post.save()
+
+    file_hex = request.FILES.get("files", None)
+    if file_hex:
+        image_url = str(post.id) + '-1.jpg'
+        with open('static/square/post_image/' + image_url, 'wb+') as f:
+            for chunk in file_hex.chunks():
+                f.write(chunk)
+
+        post.image_url = image_url
+        post.save()
+
+    response = {'submit_status': 'success'}
+    return JsonResponse(response)
