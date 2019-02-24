fcb364c713f421236f2d4342feffb1fba221ca2d
haogaofeng <gf_hao@126.com>
2019-02-24 16:23:33 +0800

    1. mrrs修改登陆功能。

--- mrrs_backend/users/views.py
+++ mrrs_backend/users/views.py
@@ -8,6 +8,12 @@ from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
 
 
+LOGIN_MESSAGE = {
+    'password_error': '用户名或密码错误，请重试',
+    'is_logout': '已退出登陆',
+}
+
+
 def login_view(request):
     if request.method == 'GET':
         next_url = request.GET.get('next')
@@ -15,20 +21,28 @@ def login_view(request):
             next_url = reverse('rooms:index')
 
         context = {'next': next_url}
-        error = request.GET.get('error')
-        if error:
-            context['error'] = error
+        message = request.GET.get('message')
+        if message:
+            context['message'] = LOGIN_MESSAGE[message]
 
         return render(request, 'users/login.html', context)
 
     elif request.method == 'POST':
-        user = authenticate(request, username=request.POST['user'], password=request.POST['password'])
+        username = request.POST['user']
+        password = request.POST['password']
+        next_url = request.POST['next']
+        user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return HttpResponseRedirect(request.POST['next'])
+            return HttpResponseRedirect(next_url)
         else:
-            return HttpResponseRedirect(reverse('users:login') + '?next=' + request.POST['next'] + '&error=1')
+            redirect_url = reverse('users:login') + '?next=' + next_url + '&message=password_error'
+            return HttpResponseRedirect(redirect_url)
 
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('users:login') + '?' + 'message=is_logout')
 
 
 
