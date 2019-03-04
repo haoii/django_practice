38e43bcb7811fe1211769ee9f2aabfd3a333d058
haogaofeng <gf_hao@126.com>
2019-03-04 18:36:59 +0800

    1. rx_account添加获取suppliers。

--- rx_account_backend/account/urls.py
+++ rx_account_backend/account/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('customers/', views.customers, name='customers'),
+    path('suppliers/', views.suppliers, name='suppliers'),
     path('add_customer/', views.add_customer, name='add_customer'),
     path('collect_from_customer/', views.collect_from_customer, name='collect_from_customer'),
     path('collections_from_customer/', views.collections_from_customer, name='collections_from_customer'),
